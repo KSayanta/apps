77761fefbd61537c742c04efa1a89b8bd057b42e
Sayanta Karmakar <sayantakarmakar1999@gmail.com>
2025-05-13 18:26:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "/app/",
+  base: "/apps/",
   build: {
     rollupOptions: {
       input: {
