c45e183b8046ca276bd76be1de7c937527d1fe34
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-14 11:22:29 +0530

    fix: unnecessary ping host

--- chef-claude/App.jsx
+++ chef-claude/App.jsx
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
-import { pingHost } from "/src/utils.js";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
-  useEffect(() => {
-    pingHost();
-  }, []);
-
   return (
     <>
       <Header />
