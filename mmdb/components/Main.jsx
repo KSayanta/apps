7c1bba2ca21ba43c056903d0bf2d29319f404a34
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-17 02:29:01 +0530

    update initial style

--- mmdb/components/Main.jsx
+++ mmdb/components/Main.jsx
@@ -1,9 +1,32 @@
+import React from "react";
+import { ReactComponent as Search } from "../../public/icons/Search.svg";
+import { ReactComponent as Reel } from "../../public/icons/Reel.svg";
 import "./Main.css";
 
 export default function Main() {
   return (
-    <>
-      <h1>This is mmdb</h1>
-    </>
+    <main className="main">
+      <section className="main__cover">
+        <h2 className="heading">Find your film</h2>
+        <a href="#" className="btn btn-alt">
+          My Watchlist
+        </a>
+      </section>
+
+      <search className="main__search">
+        <form action="" className="wrapper form">
+          <Search className="icon" />
+          <input type="search" placeholder="Search for a title" />
+          <button className="btn">Search</button>
+        </form>
+      </search>
+
+      <section className="wrapper main__content ">
+        <div className="empty">
+          <Reel className="icon" role="img" />
+          <p>Start exploring</p>
+        </div>
+      </section>
+    </main>
   );
 }
