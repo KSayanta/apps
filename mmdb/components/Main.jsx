9b31372e08e9e678a0dc5a69ee612493042958f0
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-18 10:44:59 +0530

    feat: search and display

--- mmdb/components/Main.jsx
+++ mmdb/components/Main.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import { getDatafromOmdb, getDatafromOmdbById } from "../../src/utils";
 
 import Cover from "./Cover";
 import Search from "./Search";
 import Status from "./Status";
-import MovieCard from "./MovieCard";
+import MovieCards from "./MovieCards";
 import Watermark from "./Watermark";
+
 import "./Main.css";
 
 export default function Main() {
+  // State
+  const [searchArr, setSearchArr] = useState([]);
+
+  // Derived
+  const isSearched = searchArr.length > 0;
+
+  // Handler functions
+  async function submitSearch(formData) {
+    const querry = formData.get("mainSearch");
+    const searchesArr = await getDatafromOmdb(querry).then(data => data.Search);
+
+    const showsPromises = searchesArr.map(
+      async search => await getDatafromOmdbById(search.imdbID)
+    );
+
+    setSearchArr(await Promise.all(showsPromises));
+  }
+
   return (
     <main className="main">
       <Cover />
-      <Search />
+      <Search action={submitSearch} />
 
-      <section className="hidden wrapper main__content ">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </section>
+      {!isSearched && <Watermark />}
+      {isSearched && <MovieCards cards={searchArr} />}
 
       <Status className="hidden" message="" showBtn="" />
-      <Watermark className="" />
     </main>
   );
 }
