b94c5c98cfa9a8324c1cbc5efd981d56be79677e
KSayanta <sayantakarmakar1999@gmail.com>
2025-05-27 21:56:08 +0530

    feat: add testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import Card from "./components/Card";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import { MdOutlineCloudDone } from "react-icons/md";
+
 export default function App() {
   return (
     <>
       <Header />
-      <Main>
-        <Card icon={<MdOutlineCloudDone />} title="Deveoldmend Bithd">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, non.
-          Animi at blanditiis qui quisquam modi similique, voluptatem debitis
-          deleniti ullam placeat aspernatur illo maxime, laudantium itaque
-          commodi? Voluptatibus, quos.
-        </Card>
-      </Main>
+      <Main></Main>
     </>
   );
 }
